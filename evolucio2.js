5812c3c2babf950f6ee07b4d2c518ee0a1b89fed
Pau Mata Gómez <pmata279@gmail.com>
2022-03-08 17:20:45 +0100

    node

---
+++ evolucio2.js
@@ -0,0 +1,42 @@
+let readlineSync = require("readline-sync");
+
+cometdius();
+
+conte();
+
+pregunta();
+
+
+function diusi(opcio){
+    console.log("Gegant: -a veure qui guanya!- El Gegant a la Castanya!.I cauen una, dues, tres, quatre i cinc castanyes.");
+    console.log("El gegant sacseja un altre castanyer.Gegant: -a veure qui guanya!- El Gegant a la Castanya!.I tornen a caure una, dues, tres, quatre i cinc castanyes.");
+    console.log("I així un darrere l’altre va anar sacsejant tots els castanyers. Gegant: -a veure qui guanya!- El Gegant a la Castanya!I cauen les últimes una, dues, tres, quatre i cinc castanyes.");
+    console.log("Ara la castanyera ja pot omplir el cistell de castanyes i marxa contenta cap a la plaça de la ciutat on hi té la seva paradeta. I torra les castanyes amb el gat marrameu que li fa companyia i crida: -Castanyes torrades calentes i fumades! Qui en vol?- ");
+    console.log("I fan tanta bona olor que la gent que passeja no se’n pot estar de comprar-les i s’acaben totes, totes. I si te n’ha fet venir ganes de menjar, hauràs de fer com la Castanyera i anar-ne a buscar, perquè aquest conte que tenia un gat, ja s’ha acabat!");
+
+}
+function conte(){
+console.log("Ja ha arribat la tardor! Les fulles cauen dels arbres,\nsurten els bolets i les castanyes.Com cada any la Castanyera es desperta de bon matí per anar a la muntanya a collir castanyes.Primer es posa la camisa que li va petita,després la faldilla que li fa campana, finalment les sabates que fan cloc-cloc i surt de casa contenta.Quan arriba el bosc veu els arbres carregats de castanyes, però a terra no n’hi ha ni una. Intenta sacsejar els arbres perquè caiguin,però no ho aconsegueix. Després crida el seu amic, el Gegant del Castanyer, que és molt alt i forçut.");
+}
+
+function pregunta(){
+    let opcio = readlineSync.question("Vols continua la historia? S/N ");
+
+    if(opcio==='s'){
+        continuar();
+        diusi();
+       
+       }else{
+       console.log("Adeu")
+       }
+}
+
+function continuar(){
+    console.log("Vols continuar");
+          
+}
+function cometdius(){
+let nom = readlineSync.question("Com et dius Aventure? ");
+
+console.log("Hola " + nom + ", te explicare una Historia");
+}
---
+++ evolucio3i4.js
@@ -0,0 +1,100 @@
+
+let readlineSync = require("readline-sync");
+
+const historia = [{
+    "text" :"Es una aventura per trobar el meu amic que s'ha amagat",
+    "si" : 1,
+    "no" : 2,
+},{
+    "text" : 'Vam trobar la empremta a la porta, i quan miras desde la porta del amari es veu unes pijades de fang que van cap la cuina',
+    "si" : 2,
+    "no" : 3,
+},{
+    "text" : 'Vam trobar un cabell del meu amic,pero despres vam anar al meu cuarto',
+    "si" : 3,
+    "no" : 4,
+},{
+    "text" : "Vam olora la seva colonia",
+    "si" : 4,
+    "no" : 5,
+},{
+    "text" : "Hem trobat el meu amic",
+    "si" : 5,
+    "no" : 6,
+}];
+
+let numerodePagina = 0;
+while (numerodePagina >=0){
+    let pagina = historia[indexndePagina];
+    console.log(pagina.text);
+    amicperdut();
+    si : pagina = pagina.si;
+}
+ 
+
+
+cometdius();
+
+let començament = "Es una aventura per trobar el meu amic que s'ha amagat";
+let pista = "Vam trobar la empremta a la porta, i quan miras desde la porta del amari es veu unes pijades de fang que van cap la cuina";
+let pista1 = "Vam trobar un cabell del meu amic,pero despres vam anar al meu cuarto";
+let pista2 = "Vam olora la seva colonia";
+let pista3 = "Hem trobat el meu amic";
+
+
+amicperdut();
+
+
+function amicperdut(){
+    let perdut = false;
+    let res = readlineSync.question("Vols buscar el meu amic 'S/N' ?");
+if (res === "S") {
+  console.log(començament);
+  while (!perdut) {
+    let res1 = readlineSync.question("Trobem algu en el armari?");
+    if (res1 === "S") {
+      console.log(pista);
+        perdut = true;
+    let res2 = readlineSync.question("Trobem algu a la cuina?");
+     if(res2 ==='S'){
+        console.log(pista1);
+        perdut=true;
+        let res3 = readlineSync.question("Trobem algu a la meva Habitacio?");
+        if(res3==='S'){
+            console.log(pista2);
+            perdut=true;
+            let res4 = readlineSync.question("Trobarem alu a baix del meu llit?");
+            if(res4==='S'){
+                console.log(pista3);
+                perdut=true;
+                console.log("Moltes gracies")
+               
+            }else{
+                console.log("T'has equivocat")
+                perdut=false;
+            }
+        }else{
+            console.log("T'has equivocat")
+            perdut=false;
+        }
+    }else{
+        console.log("T'has equivocat")
+        perdut=false;
+    }
+    } else {
+      console.log("Casi");
+      perdut = false;
+    
+    }
+  }
+} else {
+  console.log("Moltes gracies per res");
+}
+
+}
+function cometdius(){
+    let nom = readlineSync.question("Com et dius? ");
+
+    console.log("Hola " + nom);
+    
+}
